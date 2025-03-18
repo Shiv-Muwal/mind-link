3ec5085b187c8bd305abd9ab9fb4f8ba32aedcb2
Shiv-Muwal <shivmuwal.147@gmail.com>
2025-03-18 18:19:40 +0530

    project done

---
+++ backend/src/models/message.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose"
+
+const messageSchema = new mongoose.Schema({
+  text: {
+    type: String,
+  },
+  senderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  reciverId: { 
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  
+},{timestamps: true});
+
+const Message = mongoose.model("Message", messageSchema);
+export default Message
